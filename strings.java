1111c17efa7b06b8082ecb2d616c3f3565ca64fd
anjanreddy2420030750 <vangetianjanreddy@gmail.com>
2025-07-30 09:27:09 +0530

    Add files via upload

---
+++ strings.java
@@ -0,0 +1,18 @@
+package oops_skill;
+
+public class strings {
+	public static void main(String[] args) {
+		String s1="Anjan Reddy";
+		String s2="Anjan Reddy";
+		System.out.println(s1==s2);
+		
+		s1=s1+"varma";
+		System.out.println(s1==s2);
+		
+		String s3="Anjan Reddy";
+		
+		
+		System.out.println(s1==s3);
+		
+}
+}
